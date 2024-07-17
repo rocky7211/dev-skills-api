836dd5dfe61da233ef3f6307fbb200d5feed2782
rocky7211 <jared.mcdowall@outlook.com>
2024-07-17 16:00:59 +1200

    created MenuHandler instance in main, fixed up other files

--- app/services/menu_handler.py
+++ app/services/menu_handler.py
@@ -1,8 +1,14 @@
+from services.skills_service import SkillsService
+
+# Class to handle the menu options
 class MenuHandler:
-    def __init__(self, skill_service):
+
+    # Constructor
+    def __init__(self, skill_service: SkillsService) -> None:
         self.skill_service = skill_service
 
-    def menu(self):
+    # Method to display the menu
+    def menu(self) -> None:
         print("\nWelcome! Type the number of the option you would like to choose:")
         print("1. Add a skill")
         print("2. Find a skill")
@@ -14,27 +20,32 @@ class MenuHandler:
         user_input = input("Enter a number: ")
         if user_input == "1":
             self.add_skill_input()
+            self.menu()
         elif user_input == "2":
             self.find_skill_input()
+            self.menu()
         elif user_input == "3":
             self.decrement_skill_input()
+            self.menu()
         elif user_input == "4":
             self.remove_skill_input()
+            self.menu()
         elif user_input == "5":
             self.skill_service.get_all_skills()
+            self.menu()
         elif user_input == "6":
-            self.quit()
+            self.skill_service.quit()
         else:
             print("Invalid input. Please enter a number between 1 and 6.")
-            return self.menu()
-
+            self.menu()
+        
+    # Method to repeat the input
     def repeat_input(self, method: callable, action: str) -> None:
-        repeat = input(f"Would you like to {action} another skill? (y/n): ").lower()
+        repeat = input(f"Would yoxxu like to {action} another skill? (y/n): ").lower()
         if repeat == "y":
             method()
-        else:
-            self.menu()
 
+    # Method to get the skill name from the user
     def user_skill_input(self) -> str:
         skill_name = input("Enter the skill name: ").lower()
         if skill_name == "":
@@ -42,11 +53,13 @@ class MenuHandler:
             return self.user_skill_input()
         return skill_name
 
+    # Method to handle the add skill input
     def add_skill_input(self) -> None:
         skill_name = self.user_skill_input()
         self.skill_service.add_skill(skill_name)
         self.repeat_input(self.add_skill_input, "add")
 
+    # Method to handle the find skill input
     def find_skill_input(self) -> None:
         skill_name = self.user_skill_input()
         skill = self.skill_service.find_skill(skill_name)
@@ -56,12 +69,14 @@ class MenuHandler:
             print("Skill not found")
         self.repeat_input(self.find_skill_input, "find")
 
+    # Method to handle the decrement skill input
     def decrement_skill_input(self) -> None:
         skill_name = self.user_skill_input()
         result = self.skill_service.decrement_skill(skill_name)
         print(result)
         self.repeat_input(self.decrement_skill_input, "decrement")
 
+    # Method to handle the remove skill input
     def remove_skill_input(self) -> None:
         skill_name = self.user_skill_input()
         result = self.skill_service.remove_skill(skill_name)
--- app/services/skills_service.py
+++ app/services/skills_service.py
@@ -1,37 +1,40 @@
 from infrastructure.db_repository import SQLiteRepository
 from domain.skill import Skill
 
+# Class to handle the skills service
 class SkillsService:
 
+    # Constructor
     def __init__(self, repository: SQLiteRepository) -> None:
         self.repository = repository
 
-    def add_skill(self) -> None:
-        skillName = input("Enter the skill name: ").lower()
-        if not skillName:
-            print("Skill name cannot be empty")
-            return self.add_skill()
-        
-        skill = self.repository.get_skill(skillName)
-
+    # Method to display the menu
+    def add_skill(self, skill_name: str) -> None:
+        skill = self.repository.get_skill(skill_name)
         if skill:
             self.repository.increment_skill(skill)
         else:
-            skill = Skill(skillName)
+            skill = Skill(skill_name)
             self.repository.add_skill(skill)
-        
-        repeat = input("Would you like to decrement another skill? (y/n): ")
 
-        if repeat.lower() == "y":
-            self.add_skill()
-        else:
-            self.menu()
+    # Method to find a skill
+    def find_skill(self, skill_name: str) -> Skill:
+        skill = self.repository.get_skill(skill_name)
+        if skill:
+            return skill
+        return None
     
-    def remove_skill(self) -> None:
-        skill_name = input("Enter a skill to delete: ").lower()
-        if skill_name == "":    
-            print("Skill name cannot be empty")
-            return self.remove_skill()
+    # Method to decrement a skill
+    def decrement_skill(self, skill_name: str) -> str:
+        skill = self.repository.get_skill(skill_name)
+        if skill:
+            self.repository.decrement_skill(skill)
+            return f"Skill decremented: {skill}"
+        else:
+            return "Skill not found"
+        
+    # Method to remove a skill
+    def remove_skill(self, skill_name: str) -> str:
         skill = self.repository.get_skill(skill_name)
         if skill:
             self.repository.remove_skill(skill)
@@ -39,92 +42,16 @@ class SkillsService:
         else:
             print("Skill not found")
 
-        repeat = input("Would you like to delete another skill? (y/n): ")
-        if repeat.lower() == "y":
-            self.remove_skill()
-        else:
-            self.menu()
-
-    def decrement_skill(self) -> None:
-        skill_name = input("Enter a skill to decrement: ").lower()
-        if skill_name == "":
-            print("Skill name cannot be empty")
-            return self.decrement_skill()
-        skill = self.repository.get_skill(skill_name)
-        if skill:
-            self.repository.decrement_skill(skill)
-            print(f"Skill decremented: {skill}")
-        else:
-            print("Skill not found")
-
-        repeat = input("Would you like to decrement another skill? (y/n): ")
-        if repeat.lower() == "y":
-            self.decrement_skill()
-        else:
-            self.menu()
-    
-    """
-    ===UNNECESSARY===
-    def view_skills(self, user_id: int) -> None:
-        skills = self.repository.get_skills(user_id)
-        for skill in skills:
-            print(f"{skill.name}: {skill.count}")
-    """
-    
-    def find_skill(self) -> None:
-        user_input = input("Enter a skill to find: ").lower()
-        if user_input == "":
-            print("Skill name cannot be empty")
-            return self.find_skill()
-        skill = self.repository.get_skill(user_input)
-        if skill:
-            print(skill)
-        else:
-            print("Skill not found")
-
-        repeat = input("Would you like to find another skill? (y/n): ")
-        if repeat.lower() == "y":
-            self.find_skill()
-        else:
-            self.menu()
-
+    # Method to get all skills
     def get_all_skills(self) -> None:
         skills = self.repository.get_all_skills()
         print("All skills:")
         for skill in skills:
             print(skill)
-        self.menu()
-    
+
+    # Method to quit the application
     def quit(self) -> None:
         print("Closing connection...")
         self.repository.close_connection()
         print("Goodbye!")
         exit()
-
-    # A menu to add, decrement, delete, find, and exit
-    def menu(self):
-        print("\nWelcome! Type the number of the option you would like to choose:")
-        print("1. Add a skill")
-        print("2. Find a skill")
-        print("3. Decrement a skill")
-        print("4. Delete a skill")
-        print("5. View skills")
-        print("6. Exit")
-        user_input = input("Enter a number: ")
-
-        if user_input == "1":
-            self.add_skill()
-        elif user_input == "2":
-            self.find_skill()
-        elif user_input == "3":
-            self.decrement_skill()
-        elif user_input == "4":
-            self.remove_skill()
-        elif user_input == "5":
-            self.get_all_skills()
-        elif user_input == "6":
-            self.quit()
-        else:
-            print("Invalid input. Please enter a number between 1 and 6.")
-            return self.menu()
-
