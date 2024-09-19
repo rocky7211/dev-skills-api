d5239ff0822a92dbf144c51c4c544a6e963eda6c
rocky7211 <jared.mcdowall@outlook.com>
2024-09-19 20:44:18 +1200

    created wsgi file to replace main and moved it to root.

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,31 @@
+"""
+Description: This file is the entry point of the application.
+It creates an instance of the SkillsService class and adds it to the Flask app's configuration.
+"""
+import sys
+import os
 
-from app.main import app
+# Add the 'app' directory to the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'app')))
 
+import app.services.skills_service as service
+from api.routes import configure_routes
+from app.infrastructure.db_repository import SQLiteRepository
+from flask import Flask
+from flask_cors import CORS
+
+# Create a Flask app
+app = Flask(__name__)
+
+# Add the SkillsService instance to the app's configuration
+configure_routes(app)
+CORS(app)
+
+# Entry point of the application
 if __name__ == "__main__":
-    app.run()
+    # Create an instance of the SkillsService class
+    skills_service = service.SkillsService(SQLiteRepository())
+    app.config['skills_service'] = skills_service
+
+    # Start the Flask app
+    app.run(debug=False, host='0.0.0.0, port=8080')
