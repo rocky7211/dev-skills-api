6c7e9f6ac9f711a08007ff0b46f3c94f9bc615bc
rocky7211 <jared.mcdowall@outlook.com>
2024-11-11 13:32:24 +1300

    modified add_skill api

--- app/api/routes.py
+++ app/api/routes.py
@@ -13,8 +13,9 @@ def configure_routes(app):
         return jsonify(skills_json)
 
     # New route to add a skill
-    @app.route('/api/skills/add_skill/<string:skill_name>', methods=['POST'])
-    def add_skill(skill_name):
+    @app.route('/api/skills/add_skill', methods=['POST'])
+    def add_skill():
+        skill_name = request.json.get('skill_name')
         skills_service = current_app.config['skills_service']
         added = skills_service.add_skill(skill_name)
         if added:
